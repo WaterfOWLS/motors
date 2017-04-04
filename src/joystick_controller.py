688996775ae3ec9626bc25828258a482ed9ae68f
WaterfOWLS 2017 <WaterfOWLS2017@gmail.com>
2017-04-04 16:27:03 -0400

    changes to joystick controller

--- src/joystick_controller.py
+++ src/joystick_controller.py
@@ -6,7 +6,7 @@ joystick_controller:
 
 from motor_comm import *
 import rospy
-import sensor_msgs.Joy
+from sensor_msgs.msg import Joy
 from motors.srv import MotorPower2
 
 
@@ -20,24 +20,24 @@ safety_threshold = -1 # value safety must be at to trigger actual movement
 
 class JS_obj:
 
-  def __init__():
+  def __init__(self):
     rospy.init_node('joystick_controller')
     try:
-      motor_service = rospy.ServiceProxy('motor_power2', MotorPower2)
+      self.motor_service = rospy.ServiceProxy('motor_power2', MotorPower2)
     except rospy.ServiceException, e:
       print "MotorPower2 service call failed: %s" % e
 
-    send_power()
-
-    rospy.Subscriber('joy', Joy, joy_callback)
     self.pow1 = 0;
     self.pow2 = 0;
+    self.send_power()
 
-  def clamp(max_val, min_val, value):
+    rospy.Subscriber('joy', Joy, self.joy_callback)
+
+  def clamp(self, max_val, min_val, value):
     return (min(max_val, max(min_val, value)))
 
-  def send_power():
-    resp = motor_service(self.pow1, self.pow2)
+  def send_power(self):
+    resp = self.motor_service(self.pow1, self.pow2, 2)
     print resp
 
   def joy_callback(self, data):
@@ -45,12 +45,13 @@ class JS_obj:
     spin = data.axes[spin_axis]
 
     if data.axes[safety] == safety_threshold:
-      self.pow1 = clamp(1, -1, for_back + spin)
-      self.pow2 = clamp(1, -1, for_back - spin)
+      self.pow1 = 0.5 * self.clamp(1, -1, for_back + spin)
+      self.pow2 = 0.5 * self.clamp(1, -1, for_back - spin)
     else:
       self.pow1 = 0
       self.pow2 = 0
 
+    print self.pow1, self.pow2
     self.send_power()
  
 # end def JS_obj
